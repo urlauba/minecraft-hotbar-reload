1e932c927536bfedb76e5d044afc4753a0b83550
urlauba <austinurlauba@gmail.com>
2023-10-01 18:36:30 -0700

    implement hotbar swapping when item breaks

--- src/main/java/com/urlauba/hotbarreload/ModMain.java
+++ src/main/java/com/urlauba/hotbarreload/ModMain.java
@@ -1,8 +1,44 @@
 package com.urlauba.hotbarreload;
 
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraftforge.event.entity.player.PlayerDestroyItemEvent;
 
 @Mod("hotbarreload")
 public class ModMain {
-     
+
+    public ModMain() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onPlayerDestroyItem(PlayerDestroyItemEvent event) {
+        ItemStack damangedItem = event.getOriginal();
+
+        Player player = event.getEntity();
+        Inventory inventory = player.getInventory();
+        int replacementIndex = findItemIndexInInventory(inventory, damangedItem.getItem());
+
+        if (replacementIndex == -1) {
+            return;
+        }
+
+        int selectedIndex = inventory.selected;
+        inventory.setItem(selectedIndex, inventory.getItem(replacementIndex));
+        inventory.setItem(replacementIndex, ItemStack.EMPTY);
+    }
+
+    public int findItemIndexInInventory(Inventory inventory, Item item) {
+        for (int i = 0; i < inventory.getContainerSize(); i++) {
+            if (inventory.getItem(i).getItem() == item) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
